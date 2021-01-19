6aa153f477319829ac2df37caedaa355bf6906f3
Louise Delaigue <louisedelaigue@gmail.com>
2021-01-19 11:35:54 +0100

    file import

--- so279_uws_ph.py
+++ so279_uws_ph.py
@@ -3,3 +3,40 @@ import os
 import numpy as np
 from scipy import stats
 
+# import spreadsheet
+db = pd.read_excel('./data/UWS/UWS_datasheet.xlsx',
+                   skiprows=[1])
+
+# create list of files we want to keep
+file_list = [file for file in os.listdir('./data/UWS') if 
+                  '_'.join(file.split('_')) in db.pH_optN.values]
+
+# create loop to extract data
+data = {} # tell python this is an empty dict so we can put the tables in
+for file in file_list:
+    fname = "./data/UWS/{}/{}.txt".format(file,file)
+    data[file] = pd.read_table(fname, skiprows=22, encoding="unicode_escape")
+
+# rename headers of df inside dict and get rid off empty columns
+rn = {
+      "Date [A Ch.1 Main]":"date",
+      "Time [A Ch.1 Main]":"time",
+      " dt (s) [A Ch.1 Main]":"sec",
+      "pH [A Ch.1 Main]":"pH",
+      "Fixed Temp (?C) [A Ch.1 CompT]":"temp"
+      }
+
+for file in file_list:
+    data[file].rename(rn, axis=1, inplace=True)
+    data[file].drop(columns=["Date [Comment]",
+                    "Time [Comment]",
+                    "Comment",
+                    "Unnamed: 23",
+                    "Unnamed: 24",
+                    "Unnamed: 25",
+                    "Unnamed: 26",
+                    "Unnamed: 27",
+                    "Unnamed: 28",
+                    "Unnamed: 29"],
+                    inplace=True)
+    data[file].dropna()
