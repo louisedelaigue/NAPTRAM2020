e82a9d95590a147a84bbd1218e54399a8480a567
Louise Delaigue <louisedelaigue@gmail.com>
2021-01-20 14:36:14 +0100

    headers rename

--- so279_uws_ph.py
+++ so279_uws_ph.py
@@ -60,7 +60,7 @@ for file in chunky:
     file = file.drop([file.index[0], file.index[1]])
     file.reset_index(drop=True)
     new = {
-       "SMB.RSSMB.T_SBE38":"temp_source"
+       'SMB.RSSMB.T_SBE38':'SBE38_water_temp'
        }
     file.rename(new, axis=1, inplace=True)
     file.dropna(subset=['temp_source'], inplace=True)
@@ -68,7 +68,54 @@ for file in chunky:
 
 # create 1 df holding all cleaned up smb data
 smb = pd.concat(smb_list)
-# smb = smb.drop([smb.index[0], smb.index[1]])
+rn = {
+      'date time':'time',
+      'WeatherStation.PDWDC.Airtemperature':'WS_airtemp'
+      'WeatherStation.PDWDC.Barometric':'WS_baro',
+      'WeatherStation.PDWC.Course':'WS_course',
+      'WeatherStation.PDWC.Date':'WS_date',
+      'WeatherStation.PDWC.Heading':'WS_heading',
+      'WeatherStation.PDWC.Humidity':'WS_humidity',
+      'WeatherStation.PDWC.Latitude':'WS_lat',
+      'WeatherStation.PDWC.Longitude':'WS_lon',
+      'WeatherStation.PDWC.Longwave':'WS_longwave',
+      'WeatherStation.PDWC.NormalizedTo':'WS_normto',
+      'WeatherStation.PDWC.Pyrogeometer':'WS_pyrogeometer',
+      'WeatherStation.PDWC.SensorValue':'WS_sensorvalue',
+      'WeatherStation.PDWC.Sentence':'WS_sentence',
+      'WeatherStation.PDWC.Shortwave':'WS_shortwave',
+      'WeatherStation.PDWC.Speed':'WS_speed',
+      'WeatherStation.PDWC.Timestamp':'WS_timestamp'
+      'WeatherStation.PDWC.Watertemperature':'WS_watertemp',
+      'WeatherStation.PDWC.Winddirection_rel':'WS_winddirection_rel',
+      'WeatherStation.PDWC.Winddirection_true':'WS_winddirection_true',
+      'WeatherStation.PDWC.Windspeed_rel':'WS_windspeed_rel',
+      'WeatherStation.PDWC.Windspeed_true':'WS_windspeed_true',
+      'WeatherStation.PDWC.Windspeed_true_Bft':'WS_windspeed_true_bft',
+      'SMB.RSSMB.Chl':'chl',
+      'SMB.RSSMB.C_SBE45':'SBE_45_C',
+      'SMB.RSSMB.Date':'date',
+      'SMB.RSSMB.Delay':'delay',
+      'SMB.RSSMB.Depth':'depth',
+      'SMB.RSSMB.EW':'ew',
+      'SMB.RSSMB.Flow':'flow',
+      'SMB.RSSMB.Latitude':'lat',
+      'SMB.RSSMB.Longitude':'lon',
+      'SMB.RSSMB.Name':'smb_name',
+      'SMB.RSSMB.NS':'ns',
+      'SMB.RSSMB.RVK':'system',
+      'SMB.RSSMB.Sal_SBE45':'SBE45_sal',
+      'SMB.RSSMB.Sentence':'sentence',
+      'SMB.RSSMB.SN':'sn',
+      'SMB.RSSMB.SV_SBE45':'SBE45_sv',
+      'SMB.RSSMB.SV_insito':'insitu_sv',
+      'SMB.RSSMB.Status':'smb_status',
+      'SMB.RSSMB.SV_AML':'smb_sv_aml',
+      'SMB.RSSMB.T_SBE45':'SBE45_water_temp',
+      'SMB.RSSMB.Time':'smb_time',
+      'SMB.RSSMB.Tur':'smb_tur'
+      }
 
 # subset smb df to open more easily
 smb_small = smb[0:100]
+smb_small.reset_index(inplace=True, drop=True)
