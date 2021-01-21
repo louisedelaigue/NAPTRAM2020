35add2fa982c0d73aa0b04958d22d920c7c8ec98
Louise Delaigue <louisedelaigue@gmail.com>
2021-01-21 15:46:34 +0100

    using int insteasd of str for seconds

--- so279_uws_ph.py
+++ so279_uws_ph.py
@@ -129,15 +129,11 @@ smb_small.reset_index(inplace=True, drop=True)
 
 # create seconds column for smb time - TO EDIT WITH AN IF CONDITION (if less than 60 duplicates, then  fill with nan)
 smb_small['second'] = smb_small.groupby('time').cumcount()+1
-smb_small['second'] = smb_small.second.map("{:02}".format)
 
 # split time into hours, minutes and seconds for pyro data
 data_small['time'] = pd.to_datetime(data_small['time'], format='%H:%M:%S.%f').dt.time
-data_small['hour'] = data_small['time'].apply(lambda x: x.hour).map("{:02}".format)
-data_small['minute'] = data_small['time'].apply(lambda x: x.minute).map("{:02}".format)
-data_small['second'] = data_small['time'].apply(lambda x: x.second).map("{:02}".format)
-
-
-
+data_small['hour'] = data_small['time'].apply(lambda x: x.hour)
+data_small['minute'] = data_small['time'].apply(lambda x: x.minute)
+data_small['second'] = data_small['time'].apply(lambda x: x.second)
 
 
