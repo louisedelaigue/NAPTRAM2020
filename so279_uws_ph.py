f462a4b239fbec31bd1d721229f3adfe6d889817
Louise Delaigue <louisedelaigue@gmail.com>
2021-01-22 09:41:02 +0100

    split smb time

--- so279_uws_ph.py
+++ so279_uws_ph.py
@@ -119,7 +119,7 @@ rn = {
       }
 
 smb.rename(rn, axis=1, inplace=True)
-
+#%%
 #================================================== SUBSETS FOR TIME COMPONENT
 # subset smb df to play around
 data_small = data[0:150]
@@ -127,14 +127,30 @@ data_small.reset_index(inplace=True, drop=True)
 smb_small = smb[0:150]
 smb_small.reset_index(inplace=True, drop=True)
 
+# DATE
+smb_small['date'] = pd.to_datetime(smb_small['time'], format='%m/%d/%Y %H:%M').dt.date
+smb_small['month'] = smb_small['date'].apply(lambda x: x.month)
+smb_small['day'] = smb_small['date'].apply(lambda x: x.day)
+
+# TIME
+smb_small['hms'] = pd.to_datetime(smb_small['time'], format='%m/%d/%Y %H:%M').dt.time
+smb_small['hour'] = smb_small['hms'].apply(lambda x: x.hour)
+smb_small['minute'] = smb_small['hms'].apply(lambda x: x.minute)
+
 # create seconds column for smb time - TO EDIT WITH AN IF CONDITION (if less than 60 duplicates, then  fill with nan)
 smb_small['second'] = smb_small.groupby('time').cumcount()+1
+
 
 # split time into hours, minutes and seconds for pyro data
 data_small['time'] = pd.to_datetime(data_small['time'], format='%H:%M:%S.%f').dt.time
 data_small['hour'] = data_small['time'].apply(lambda x: x.hour)
 data_small['minute'] = data_small['time'].apply(lambda x: x.minute)
 data_small['second'] = data_small['time'].apply(lambda x: x.second)
+
+
+
+
+#%%
 
 # merge pH data with metadata
 data_all = smb_small.merge(right=data_small,
