c2ce9784b5e258da6334040ed2d7fc9cb1a65328
Louise Delaigue <louisedelaigue@gmail.com>
2021-02-10 15:21:40 +0100

    plot init

--- so279_stn_ph_sla_sst_sla.py
+++ so279_stn_ph_sla_sst_sla.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import xarray as xr
+from matplotlib import pyplot as plt
 
 # import pH and SMB data
 df = pd.read_csv('./data/UWS/df_carb.csv')
@@ -9,4 +10,10 @@ sladat = xr.open_dataset('./data/dataset-duacs-nrt-global-merged-allsat-phy-l4_1
 
 # import satellite data for sst and sal
 bgcdat = xr.open_dataset('./data/global-analysis-forecast-phy-001-024_1612963010439.nc')
+
+#%% make plot
+# create figure
+fig, axs = plt.subplots(
+    dpi=300, figsize=(6,6), nrows=2, ncols=2
+    )
 
