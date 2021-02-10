2cf3142bf79fb6438a93d61d1563bd1a60e049d1
Louise Delaigue <louisedelaigue@gmail.com>
2021-02-10 17:00:34 +0100

    Update so279_pHinsitu_track.py

--- so279_pHinsitu_track.py
+++ so279_pHinsitu_track.py
@@ -7,12 +7,6 @@ import cmocean
 # import pH and SMB data
 df = pd.read_csv('./data/UWS/df_carb.csv')
 
-# import satellite data for sea level anomaly
-sladat = xr.open_dataset('./data/dataset-duacs-nrt-global-merged-allsat-phy-l4_1612878616230.nc')
-
-# import satellite data for sst and sal
-bgcdat = xr.open_dataset('./data/global-analysis-forecast-phy-001-024_1612963010439.nc')
-
 #%% make plot
 # create figure
 fig = plt.figure(dpi=300)
