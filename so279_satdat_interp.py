ca2a43a26aa323223b4a3b3a85c37e5c7edba396
Louise Delaigue <louisedelaigue@gmail.com>
2021-02-12 15:45:24 +0100

    interpolate + graph

--- so279_satdat_interp.py
+++ so279_satdat_interp.py
@@ -1,15 +1,22 @@
 import xarray as xr, pandas as pd
 from scipy.interpolate import interpn
 from matplotlib import dates as mdates
+from matplotlib import pyplot as plt
 
 # import GLOBAL OCEAN GRIDDED L4 SEA SURFACE HEIGHTS AND DERIVED VARIABLES NRT
 sla = xr.open_dataset('./data/dataset-duacs-nrt-global-merged-allsat-phy-l4_1612878616230.nc')
+
+
+# import GLOBAL_ANALYSIS_FORECAST_PHY_001_024
+bgcdat = xr.open_dataset('./data/global-analysis-forecast-phy-001-024_1612963010439.nc')
+
+# import cruise data
 ph = pd.read_csv('./data/UWS/df_carb.csv')
+
+# put data into matplotlib time friendly format
 ph['date_time'] = pd.to_datetime(ph.date_time)
 ph['datenum'] = mdates.date2num(ph.date_time)
 
-# # import GLOBAL_ANALYSIS_FORECAST_PHY_001_024
-# bgcdat = xr.open_dataset('./data/global-analysis-forecast-phy-001-024_1612963010439.nc')
 
 # phx = (
 #     ph[["lat_dd", "lon_dd"]]
@@ -26,8 +33,23 @@ ph['datenum'] = mdates.date2num(ph.date_time)
 # sla.isel(time=0).sla.plot()
 # sla.isel(time=0).sla.interp(latitude=[40, 41], longitude=[-20, -21, -20.5])
 
+# 3D linear interpolation along latitude, longitude and time
 ph['sla'] = interpn(
-    (mdates.date2num(sla.time.data), sla.latitude.data, sla.longitude.data),
+    (mdates.date2num(sla.time.data), sla.latitude.data, sla.longitude.data), # this needs to be same order as dimensions order in xarray
     sla.sla.data,
-    (ph.datenum, ph.lat_dd, ph.lon_dd),
+    (ph.datenum, ph.lat_dd, ph.lon_dd), # this needs to match above order
 )
+
+# make a plot
+fig, ax = plt.subplots(dpi=300)
+ax.scatter(ph.datenum, ph.SBE45_sal, s=2, c='xkcd:aquamarine', zorder=2)
+ax.set_xlabel('Time')
+ax.set_ylabel('Salinity')
+
+ax2 = ax.twinx() # command for second y axis for temp
+
+ax2.scatter(ph.datenum, ph.sla, s=2, c='xkcd:true blue', zorder=1)
+ax2.set_ylabel('Sea level anomaly / m')
+
+plt.tight_layout()
+plt.show()
